20522ca218c41c7f544caed0ee71911eebdf4ca2
triple-s-rank <specialforwar@icloud.com>
2022-06-23 15:56:48 +0300

    add datetime fileds to PokemonEntity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -13,3 +13,6 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     lat = models.FloatField()
     lon = models.FloatField()
+    appeared_at = models.DateTimeField(default=None)
+    disappeared_at = models.DateTimeField(default=None)
+
