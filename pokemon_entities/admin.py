5e5264d4f6df7fd4f0a6174cef5c4df7e7311645
triple-s-rank <specialforwar@icloud.com>
2022-06-23 15:36:30 +0300

    add PokemonEtity model

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from pokemon_entities.models import Pokemon
+from pokemon_entities.models import Pokemon, PokemonEtity
 
 admin.site.register(Pokemon)
+admin.site.register(PokemonEtity)
